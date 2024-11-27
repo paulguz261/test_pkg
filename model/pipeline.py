b1fbc2b29452ea1a12dc9193e07cce2b8f970401
paulguz261 <paulguz261@gmail.com>
2024-11-26 21:16:09 -0500

    primer commit

---
+++ model/pipeline.py
@@ -0,0 +1,54 @@
+from feature_engine.selection import DropFeatures
+from sklearn.pipeline import Pipeline
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.combine import SMOTEENN
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from xgboost import XGBClassifier
+
+from model.config.core import config
+from model.processing import features as pp
+
+clf_pipeline = Pipeline(
+    [
+        # Drop features 
+        ("drop_features", 
+        DropFeatures(
+            features_to_drop=[config.model_config.temp_features]
+            )
+        ),
+        Mappers
+        (
+           "mapper_qual",
+           pp.Mapper(
+               variables=config.model_config.qual_vars,
+               mappings=config.model_config.qual_mappings,
+           ),
+        ),
+        # Balanceo de clases
+        ("balanceo", SMOTE(sampling_strategy=0.5)
+         ),
+        # Scaler
+        ("scaler", StandardScaler()
+         ),
+        # PCA
+        ("PCA", PCA(n_components=0.95)
+         ),
+        # XGBoost
+        ("XGBoost",
+            XGBClassifier(
+                objective=config.model_config.objective,
+                n_estimators=config.model_config.n_estimators,
+                max_depth=config.model_config.max_depth,
+                learning_rate=config.model_config.learning_rate,
+                subsample=config.model_config.subsample,
+                colsample_bytree=config.model_config.colsample_bytree,
+                min_child_weight=config.model_config.min_child_weight,
+                reg_lambda=config.model_config.reg_lambda,
+                reg_alpha=config.model_config.reg_alpha,
+            ),
+        ),
+    ]
+)
---
+++ model/processing/validation.py
@@ -0,0 +1,55 @@
+from typing import List, Optional, Tuple
+
+import numpy as np
+import pandas as pd
+from pydantic import BaseModel, ValidationError
+
+from model.config.core import config
+
+
+def drop_na_inputs(*, input_data: pd.DataFrame) -> pd.DataFrame:
+    """Check model inputs for na values and filter."""
+    validated_data = input_data.copy()
+    new_vars_with_na = [
+        var
+        for var in config.model_config.features
+        if validated_data[var].isnull().sum() > 0
+    ]
+    validated_data.dropna(subset=new_vars_with_na, inplace=True)
+
+    return validated_data
+
+
+def validate_inputs(*, input_data: pd.DataFrame) -> Tuple[pd.DataFrame, Optional[dict]]:
+    """Check model inputs for unprocessable values."""
+
+    relevant_data = input_data[config.model_config.features].copy()
+    validated_data = drop_na_inputs(input_data=relevant_data)
+    errors = None
+
+    try:
+        # replace numpy nans so that pydantic can validate
+        MultipleDataInputs(
+            inputs=validated_data.replace({np.nan: None}).to_dict(orient="records")
+        )
+    except ValidationError as error:
+        errors = error.json()
+
+    return validated_data, errors
+
+
+class DataInputSchema(BaseModel):
+    gender: Optional[str]
+    age: Optional[int]
+    hypertension: Optional[int]
+    heart_disease: Optional[int]
+    ever_married: Optional[int]
+    work_type: Optional[str]
+    Residence_type: Optional[str]
+    avg_glucose_level: Optional[float]
+    bmi: Optional[float]
+    smoking_status: Optional[int]
+
+
+class MultipleDataInputs(BaseModel):
+    inputs: List[DataInputSchema]
